a1138f1c18dc4f61c5b163479669536081c36432
Sergei Nesteruk <yarench228@gmail.com>
2022-11-10 02:56:51 +0100

    serrialize

---
+++ Benchmark.h
@@ -0,0 +1,13 @@
+#pragma once
+class Benchmark
+{
+public:
+	Benchmark() = default;
+	Benchmark& operator=(const Benchmark&) = default;
+	~Benchmark() = default;
+
+	
+private:
+
+};
+
---
+++ main.cpp
@@ -0,0 +1,14 @@
+﻿#include "SomeClass.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    SomeClass obj1;
+    for (size_t i = 0; i < obj1.serialize("some_file.txt").size(); ++i)
+        cout << obj1.serialize("some_file.txt").at(i);
+    
+    return 0;
+}
