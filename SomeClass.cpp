a863f0e9176794483c7e7a757e2ee843f0b83a76
Sergei Nesteruk <yarench228@gmail.com>
2022-11-10 03:09:11 +0100

    first commit

---
+++ SomeClass.cpp
@@ -0,0 +1,94 @@
+﻿#include "SomeClass.h"
+
+#include <charconv>
+#include <fstream>
+#include <iostream>
+
+SomeClass::SomeClass(){}
+
+std::vector<std::size_t> SomeClass::serialize(const char* pathToFile)
+{
+    std::ifstream fin(pathToFile);
+    
+    unsigned char c;
+    while(fin.read(reinterpret_cast<char*>(&c),0x01))
+        for (int i = 0; i < 8; ++i)
+            binary.at(i) = ((c>>i) & 0x01);//
+    fin.close();
+    
+    return binary;
+}
+
+// const char* SomeClass::deserialize(const std::string& file)
+// {
+//     
+// }
+
+//setters
+
+void SomeClass::setInt(int number)
+{
+    this->variableInt = number;
+}
+
+void SomeClass::setDouble(double number)
+{
+     this->variableDouble = number;
+}
+
+void SomeClass::setFloat(float number)
+{
+    this->variableFloat = number;
+}
+
+void SomeClass::setStr(const std::string str)
+{
+    this->variableStr = str;
+}
+
+void SomeClass::setFunc(std::function<void()> func)
+{
+    this->variableFunc = func;
+}
+
+void SomeClass::setVec(std::vector<int> vec)
+{
+    this->variableVector = variableVector;
+}
+
+//getters
+
+int SomeClass::getInt() const
+{
+    return variableInt;
+}
+
+double SomeClass::getDouble() const
+{
+    return variableDouble;
+}
+
+float SomeClass::getFloat() const
+{
+    return variableFloat;
+}
+
+std::string SomeClass::getStr() const
+{
+    return variableStr;
+}
+
+std::function<void()> SomeClass::getFunction() const
+{
+    return variableFunc;
+}
+
+std::vector<int> SomeClass::getVecor() const
+{
+    return variableVector;
+}
+
+
+
+
+
---
+++ SomeClass.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include <string>
+#include <functional>
+#include <vector>
+
+class SomeClass final
+{
+public:
+    SomeClass();
+    SomeClass& operator=(const SomeClass&) = default;
+    ~SomeClass() = default;
+
+    std::vector<std::size_t>serialize   (const char* pathToFile);
+    const char*             deserialize (const char* file);
+
+    //setters
+    int                     getInt() const;
+    double                  getDouble() const;
+    float                   getFloat() const;
+    std::string             getStr() const;
+    std::function<void()>   getFunction() const;
+    std::vector<int>        getVecor() const;
+    //getters
+    void                    setInt       (int number);
+    void                    setDouble    (double number);
+    void                    setFloat     (float number);
+    void                    setStr       (const std::string str);
+    void                    setFunc      (std::function<void()> func);
+    void                    setVec       (std::vector<int> vec);
+    
+    
+private:
+    int                         variableInt;
+    double                      variableDouble;
+    float                       variableFloat;
+                                
+    std::string                 variableStr;
+    std::function<void()>       variableFunc;
+    std::vector<int>            variableVector;
+    std::vector<std::size_t>    binary;
+};
